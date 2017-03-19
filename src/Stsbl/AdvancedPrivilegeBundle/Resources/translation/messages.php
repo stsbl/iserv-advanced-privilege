b9546d3aa97e3801d3545189a34676b11b214c22
Felix Jacobi <felix.jacobi@stsbl.de>
2017-03-19 17:24:21 +0100

    Updated i18n.

---
+++ src/Stsbl/AdvancedPrivilegeBundle/Resources/translation/messages.php
@@ -0,0 +1,52 @@
+<?php
+// src/Stsbl/AdvancedPrivilegeBundle/Resources/translation/messages.php
+
+/* 
+ * The MIT License
+ *
+ * Copyright 2017 Felix Jacobi.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/**
+ * @author Felix Jacobi <felix.jacobi@stsbl.de>
+ * @license MIT license <https://opensource.org/licenses/MIT>
+ */
+
+// Alerts
+_('Added one group flag to one group.');
+_('Added %s group flags to one group.');
+_('Added one group flag to %s groups.');
+_('Added %s group flags to %s groups.');
+
+_('Removed one group flag from one group.');
+_('Removed %s group flags from one group.');
+_('Removed one group flag from %s groups.');
+_('Removed %s group flags from %s groups.');
+
+_('Added one privilege to one group.');
+_('Added %s privileges to one group.');
+_('Added one privilege to %s groups.');
+_('Added %s privileges to %s groups.');
+
+_('Removed one privilege from one group.');
+_('Removed %s privileges from one group.');
+_('Removed one privilege from %s groups.');
+_('Removed %s privileges from %s groups.');
